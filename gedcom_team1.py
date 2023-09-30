614696416b00ddc735047131740498410ad34aa5
Kartik Gaglani <gaglanikartik@gmail.com>
2023-09-30 17:31:13 -0400

    US22 and US25 added

--- gedcom_team1.py
+++ gedcom_team1.py
@@ -1,137 +1,147 @@
 
 from tabulate import tabulate
+
 from datetime import date, datetime
 
-# This is statement is required by the build system to query build infos
-if __name__ == '__build__':
+
+if __name__ == '_build_':
 	raise Exception;
 
 import sys;
 
-# creates a new "person" object
 class Individual:
 	def __init__(self, id):
-		self.info = {'ID': id} # dict to save individual information
+		self.info = {'ID': id} 
 
-
-# creates a new "family" object
 class Family:
 	def __init__(self, id):
-		self.info = {'ID': id} # dict to save family information
-	   
+		self.info = {'ID': id} 
 
-# all the valid tags for the project, along with their corresponding levels
 validTags = {
 	'0': [{'HEAD', 'TRLR', 'NOTE'}, {'INDI', 'FAM'}],
 	'1': [{'NAME', 'SEX', 'BIRT', 'DEAT', 'FAMC', 'FAMS'}, {'MARR', 'HUSB', 'WIFE', 'CHIL', 'DIV'}],
 	'2': {'DATE'},
 };
 
-# list of all individuals
-indis = []
-# list of all families
+individual = []
+
 fams = []
 
-# read and evalute if each new line is in valid GEDCOM format
-def readLine(fileLine):
-	args = fileLine.split();
-	
+individual_id = []
 
-	# end of file check
-	if not args: 
+family_id = []
+
+errors = []
+
+current_date = date.today()
+
+def read_line(fileLine):
+	arguments = fileLine.split();
+
+	if not arguments: 
 		return;
 
-	# valid level check (cannot go past level 2)
-	if args[0] not in validTags:
-		print(validOutput(fileLine, args, 'N'))
+	if arguments[0] not in validTags:
+		print(check_validity(fileLine, arguments, 'N'))
 		raise Exception("Please provide a valid level number") 
 
-	if args[0] == '0':
+	if arguments[0] == '0':
 
-		if args[1] in validTags[args[0]][0]:
-			return # these tags aren''t needed
-		
-		# individual and family id tags have a different format
-		elif args[2] in validTags[args[0]][1]:
-			if args[2] == 'INDI':
-				indi = Individual(args[1]) # creates new individual object with id specified by args[1]
-				indis.append(indi.info) # add object dict to list of individuals
+		if arguments[1] in validTags[arguments[0]][0]:
+			return 
+
+		elif arguments[2] in validTags[arguments[0]][1]:
+			if arguments[2] == 'INDI':
+				#US-22 Unique IDs
+				if arguments[1] not in individual_id: 
+					print(arguments[1])
+					ind = Individual(arguments[1]) 
+					individual.append(ind.info) 
+					individual_id.append(arguments[1])
+				else:
+					errors.append("Individual IDs are duplicate. Please provide correct ID.")
 			else:
-				fam = Family(args[1]) # creates new family object with id specified by args[1]
-				fams.append(fam.info) # add object dict to list of families
+			
+				if arguments[1] not in family_id:
+					family_people = Family(arguments[1]) 
+					fams.append(family_people.info) 
+					family_id.append(arguments[1])
+				else:
+					raise Exception("Family IDs are duplicate. Please provide correct ID.")
 
-		# valid tag check (must be a tag specified in valid tags)
+	
 		else:
-			print(validOutput(fileLine, args, 'N'))
+			print(check_validity(fileLine, arguments, 'N'))
 			raise Exception("Please provide a valid id tag in the proper format") 
 
-	elif args[0] == '1':
+	elif arguments[0] == '1':
+		if arguments[1] in validTags[arguments[0]][0]: 
+			
+			if arguments[1] == 'FAMS':
+				if arguments[1] not in individual[len(individual)-1]:
+					individual[len(individual)-1][arguments[1]] = [' '.join(arguments[2:])]
+				else:
+					individual[len(individual)-1][arguments[1]].append(' '.join(arguments[2:]))
+			elif len(arguments) > 2:
+				individual[len(individual)-1][arguments[1]] = ' '.join(arguments[2:]) 
 
-		if args[1] in validTags[args[0]][0]: # these tags specify certain information for the individual it describes 
-			# edge case - FAMS information should be a list of strings since you can have multiple spouces
-			if args[1] == 'FAMS':
-				if args[1] not in indis[len(indis)-1]:
-					indis[len(indis)-1][args[1]] = [' '.join(args[2:])]
-				else:
-					indis[len(indis)-1][args[1]].append(' '.join(args[2:]))
-			elif len(args) > 2:
-				indis[len(indis)-1][args[1]] = ' '.join(args[2:]) # store the infomation for the most recent person added to the "indis" list
-			
 			else:
-				# edge case - information is within a nested date tag and not on the same line, save the tag for later
-				indis.append(args[1])
-				
-		elif args[1] in validTags[args[0]][1]: # these tags specify certain information for the family it describes 
-			
-			# edge case - FAMS information should be a list of strings since you can have multiple spouces
-			if args[1] == 'CHIL':
-				if args[1] not in fams[len(fams)-1]:
-					fams[len(fams)-1][args[1]] = [' '.join(args[2:])]
+				individual.append(arguments[1])		
+		elif arguments[1] in validTags[arguments[0]][1]: 
+			if arguments[1] == 'CHIL':
+				if arguments[1] not in fams[len(fams)-1]:
+					fams[len(fams)-1][arguments[1]] = [' '.join(arguments[2:])]
 				else:
-					fams[len(fams)-1][args[1]].append(' '.join(args[2:]))
-			elif len(args) > 2:
-				fams[len(fams)-1][args[1]] = ' '.join(args[2:]) # store the infomation for the most recent family added to the "fams" list
+					fams[len(fams)-1][arguments[1]].append(' '.join(arguments[2:]))
+			elif len(arguments) > 2:
+				fams[len(fams)-1][arguments[1]] = ' '.join(arguments[2:]) 
 			else:
-				# edge case - information is within a nested date tag and not on the same line, save the tag for later
-				fams.append(args[1]) 
-		
-		# valid tag check (must be a tag specified in valid tags)
+				fams.append(arguments[1]) 
 		else:
-			print(validOutput(fileLine, args, 'N'))
+			print(check_validity(fileLine, arguments, 'N'))
 			raise Exception("Please provide a valid tag in the proper format") 
-
-	elif args[1] in validTags[args[0]]:
-		# date tag - take the previous saved tag and add date information to the previously saved object in array
-		if isinstance(indis[len(indis)-1], str):
-			tag = indis.pop()
-			indis[len(indis)-1][tag] = ' '.join(args[2:])
+	elif arguments[1] in validTags[arguments[0]]:
+		date = covert_date(' '.join(arguments[2:]))
+		if isinstance(individual[len(individual)-1], str): 
+			tag = individual.pop()
+			individual[len(individual)-1][tag] = date
+			
 		elif isinstance(fams[len(fams)-1], str):
 			tag = fams.pop()
-			fams[len(fams)-1][tag] = ' '.join(args[2:])
-		# valid format check (date must proceed a level 1 tag)
+			fams[len(fams)-1][tag] = date
+
 		else:
-			print(validOutput(fileLine, args, 'N'))
+			print(check_validity(fileLine, arguments, 'N'))
 			raise Exception("Please make sure your DATE tag belongs to a valid tag") 
 	else:
-		print(validOutput(fileLine, args, 'N'))
+		print(check_validity(fileLine, arguments, 'N'))
 		raise Exception("Improper GEDCOM format") 	
 
-	#print(validOutput(fileLine, args, 'Y'))
 	return
 
 
-# print the input and output of each new line 
-def validOutput(fileLine, args, isValid):
+
+#US25
+def family_output(names):
+	first_names = []
+	for name in names :
+			names = name.split(" ")
+			first_names.append(names[0])
+
+	res = {x for x in first_names if first_names.count(x) > 1}
+	if res :
+		raise Exception("First names of individuals cannot be same.")
+	
+
+def check_validity(fileLine, args, is_valid):
 	returnOutput = "--> {0}\n".format(fileLine.rstrip('\r\n'));
 	returnOutput += "<-- ";
-
-	# print output in the format "<-- <level>|<tag>|<valid?> : Y or N|<arguments>"
 	for i in range(len(args)):
 		returnOutput += args[i];
 		if i == 0:
 			returnOutput += "|";
 		elif i == 1:
-			returnOutput += "|{}|".format(isValid)
+			returnOutput += "|{}|".format(is_valid)
 			if len(args) == 2:
 				returnOutput += "\n";
 		elif i < len(args)-1:
@@ -141,79 +151,115 @@ def validOutput(fileLine, args, isValid):
 			
 	return returnOutput;
 
-# calculates an individual's age and whether they are alive, adds this to their dictionary
-def getAge(today, personInfo):
-	# print(personInfo)
-	birth = datetime.strptime(personInfo["BIRT"], '%d %b %Y').date()
-	if 'DEAT' in personInfo:
-		death = datetime.strptime(personInfo["DEAT"], '%d %b %Y').date()
-		personInfo["AGE"] = death.year - birth.year - ((death.month, death.day) < (birth.month, birth.day))
-		personInfo["ALIVE"] = False
+def covert_date(date):
+	try:
+		return datetime.strptime(date, '%d %b %Y').date()
+	except:
+		raise Exception("Date should be in the format 'Day Month Year'")
+
+
+def age(today_date, person_information):
+	birth = person_information["BIRT"]
+	if 'DEAT' in person_information:
+		death = person_information["DEAT"]
+		person_information["AGE"] = death.year - birth.year - ((death.month, death.day) < (birth.month, birth.day))
+		person_information["ALIVE"] = False
 	else:
-		personInfo["AGE"] = today.year - birth.year - ((today.month, today.day) < (birth.month, birth.day))
-		personInfo["ALIVE"] = True
+		person_information["AGE"] = today_date.year - birth.year - ((today_date.month, today_date.day) < (birth.month, birth.day))
+		person_information["ALIVE"] = True
 		
-	return personInfo
+	return person_information
 
-# binary search through the list of individuals to find the matching ID
-def searchID (high, low, target):
+def search_id(array, high, low, target):
 	if high <= low:
-		return False
+		raise Exception("ID " + target + " not found.") 
 
-	mid = (high+low)//2
+	mid = high + (low-high)//2
 
-	# turn midpoint string ID into a number
-	midNum = int(''.join(filter(str.isdigit, indis[mid]['ID'])))
+	mid_ = int(''.join(filter(str.isdigit, array[mid]['ID'])))
 
-	if midNum > target:
-		return searchID(mid, low, target)
-	if midNum < target:
-		return searchID(high, mid+1, target)
-	return indis[mid]["NAME"]
+	if mid_ > target:
+		return search_id(array, mid, low, target)
+	if mid_ < target:
+		return search_id(array, high, mid+1, target)
+	return array[mid]
 
 
-# main method
+
 def init():
 	try:
-		filename = input("Please enter the name of the file: ")
+		filename = input("Please enter the name of the file (defaults to test3.ged if no file given): ")
 		if(filename==""):
-			filename="test3.ged"
-		with open(filename, 'r',encoding='utf-8-sig') as infile:
+			filename="test.ged"
+		with open(filename, 'r', encoding='utf-8-sig') as infile:
 			for line in infile:
-				readLine(line);
+				#print(line)
+				read_line(line);
 	except FileNotFoundError:
 		print ('''
 		ERROR: GEDCOM file does not exist.
 		''');
 		sys.exit();
 
-	# sort each list by their ID nums
-	indis.sort(key=lambda info: int(''.join(filter(str.isdigit, info["ID"])))) 
-	fams.sort(key=lambda info: int(''.join(filter(str.isdigit, info["ID"])))) 
+	individual.sort(key=lambda info: int(''.join(filter(str.isdigit, info["ID"]))))
+	fams.sort(key=lambda info: int(''.join(filter(str.isdigit, info["ID"]))))
 
-	currDate = date.today()
-	for person in indis:
-		person = getAge(currDate, person)
+	
+	for per in individual:
+		per = age(current_date, per)
 
 	for family in fams:
-		# turn husband string ID into a number
-		husbID = int(''.join(filter(str.isdigit, family["HUSB"])))
-		husbName = searchID(len(indis), 0, husbID)
+		
+		family_names = []
+		
+		husbandID = int(''.join(filter(str.isdigit, family["HUSB"])))
+		husband = search_id(individual, len(individual), 0, husbandID)
+		if not husband:
+			raise Exception("Husband ID must exist.")
+		else:
+			family_names.append(husband['NAME'])
 
-		# turn wife string ID into a number
-		wifeID = int(''.join(filter(str.isdigit, family["WIFE"])))
-		wifeName = searchID(len(indis), 0, wifeID)
+		wife_id = int(''.join(filter(str.isdigit, family["WIFE"])))
+		wife = search_id(individual, len(individual), 0, wife_id)
+		if not wife:
+			raise Exception("Wife ID must exist.")
+		else:
+			family_names.append(wife['NAME'])
 
-		if not husbName or not wifeName: # ID not found in list
-			print(wifeID, husbID)
-			raise Exception("Family Husband and Wife IDs must exist.")
+		family["HUSB NAME"] = husband["NAME"]
+		family["WIFE NAME"] = wife["NAME"]
+        
+		
+		if "CHIL" in family:
+			for chil_str in family["CHIL"]:
+				child_id = int(''.join(filter(str.isdigit, chil_str)))
+				child = search_id(individual, len(individual), 0, child_id)
+				if not child:
+					raise Exception("Wife ID must exist.")
+				else:
+					family_names.append(child['NAME'])
+	
+				childBirthdate = child["BIRT"]
 
-		family["HUSB NAME"] = husbName
-		family["WIFE NAME"] = wifeName
 
-	print(tabulate(indis, headers = "keys"))
-	print()
-	print(tabulate(fams, headers = "keys"))
+			
+		family_output(family_names)
+
+	outfile = open(filename + ".txt", "w")
+
+	outfile.write(tabulate(individual, headers = "keys", tablefmt="github"))
+	outfile.write('\n\n')
+	outfile.write(tabulate(fams, headers = "keys", tablefmt="github"))
+	outfile.write('\n\n')
+
+	
+	outfile.write('ERRORS\n')	
+	for err in errors:
+		outfile.write(err)
+		outfile.write('\n')	
+
+	outfile.close()
+
 
 init()
 sys.exit();
